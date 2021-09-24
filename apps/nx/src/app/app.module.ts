fa810a5f490f7ce2c1c70a3b23c82905b5238cc1
Sam Lin <saml@millerslab.com>
2021-09-24 12:00:45 -0500

    update app.module

--- apps/nx/src/app/app.module.ts
+++ apps/nx/src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CoreModule } from '@org/core';
+import { UiModule } from '@org/ui';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule.withServerTransition({ appId: 'serverApp' })],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    CoreModule,
+    UiModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
