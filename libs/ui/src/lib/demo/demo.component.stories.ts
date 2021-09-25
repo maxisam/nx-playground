0e0577a0a8a242cca528b9065d47b227c785ca4b
Sam Lin <saml@millerslab.com>
2021-09-25 04:16:27 -0500

    Generating Storybook Configuration
    ng generate @nrwl/angular:storybook-configuration --name=ui

---
+++ libs/ui/src/lib/demo/demo.component.stories.ts
@@ -0,0 +1,15 @@
+
+import { DemoComponent } from './demo.component';
+
+export default {
+  title: 'DemoComponent',
+  component: DemoComponent
+}
+
+export const primary = () => ({
+  moduleMetadata: {
+    imports: []
+  },
+  props: {
+  }
+})
